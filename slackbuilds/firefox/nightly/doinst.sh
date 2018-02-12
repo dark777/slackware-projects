75b49729b6d5a709a6520b08517a628385ddf71e
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-11 22:39:12 -0200

    Packages e Settings

--- slackbuilds/firefox/nightly/doinst.sh
+++ slackbuilds/firefox/nightly/doinst.sh
@@ -1,4 +1,4 @@
 ( cd usr/bin ; rm -rf firefox )
-( cd usr/bin ; ln -sf /usr/lib64/firefox-59.0b8/firefox firefox )
+( cd usr/bin ; ln -sf /usr/lib64/firefox-56.0b2/firefox firefox )
 ( cd usr/share/pixmaps ; rm -rf firefox.png )
 ( cd usr/share/pixmaps ; ln -sf /usr/share/icons/hicolor/256x256/apps/firefox.png firefox.png )
